4a6e43cc6f6947b80b77957bb678ca2bd4f8b1a9
Jigz Andrei Gaerlan <jigzgaerlan@gmail.com>
2025-09-22 14:12:48 +0800

    Switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+  import { HashRouter, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+<HashRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+  </Routes>
+</HashRouter>
+
 );
